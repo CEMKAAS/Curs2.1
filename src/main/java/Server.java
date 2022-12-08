3211ab8197d9667bd006e2539bc461678574d3a4
CEMKAAS-Laptop <zaroslikob@mail.ru>
2022-12-08 09:22:00 +0300

    фиксы

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -15,7 +15,6 @@ import java.util.Map;
 public class Server {
 
     private MaxCategory maxCategory = new MaxCategory();
-    private CategoryMaxJson categoryMaxJson;
 
     public void serverStart() {
 
